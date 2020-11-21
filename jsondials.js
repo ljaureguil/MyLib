7006867e6a2cf96cf048faf75d1b9d0c0b60e06d
ljaureguil <33967760+ljaureguil@users.noreply.github.com>
2020-11-21 14:00:47 -0800

    Update jsondials.js

--- jsondials.js
+++ jsondials.js
@@ -90,3 +90,25 @@
         }
         xhr.send(null);
         }
+function loadDoc(url,callback) {
+  var xhttp = new XMLHttpRequest();
+  
+  xhttp.open("GET", url, false);
+  xhttp.send();
+callback(xhttp.responseText);
+
+}
+function showmsg(m){
+  alert("parece que si se pudo" + m);
+}
+
+    function getPos(callback) {
+      navigator.geolocation.getCurrentPosition((position) => {
+          var ob = {
+              latitud: position.coords.latitude,
+              longitud: position.coords.longitude
+          };
+          callback(ob)
+      })
+  }                            
+
